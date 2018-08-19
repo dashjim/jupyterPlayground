ae6a7bf78124b832745cfd4854e9d572f6d32e6c
dashjim <dashjim@gmail.com>
2018-08-19 11:26:23 +0800

    env specific change.

--- sankeyChart/scheduler.sh
+++ sankeyChart/scheduler.sh
@@ -4,8 +4,8 @@ while [ true ]; do
  sleep 3
  # do what you need to here
 
- python read_from_mysql.py
- python python_sankey_01.py
+ python3 read_from_mysql.py
+ python3 python_sankey_01.py
  svg-sankey --size 1920,1080 --margins 10,150 ivr_rec_sankey.json > ivr_rec_sankey.svg
  python remove_header.py
 done
